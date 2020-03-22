3c8c14372da23ce3398efea22ccbae8da5c35e07
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-22 14:01:29 -0300

    format data

--- index.js
+++ index.js
@@ -14,9 +14,72 @@ const apiUrl = 'https://covid-api.mmediagroup.fr/v1';
 
 const fs = require('fs');
 const path = require('path');
+const countriesInfo = require('./countries_info.json');
 
 function getFilePath(url) {
     return path.join('cache', `${url.replace(/[\W_]+/g, "_")}.json`);
+}
+
+function normalizedName(countryName) {
+    switch (countryName) {
+        case 'Korea, South': return 'Republic of Korea'; break;
+        case 'US': return 'United States of America'; break;
+        case 'Czechia': return 'Czech Republic'; break;
+        case 'Taiwan*': return 'Taiwan'; break;
+        case 'Sri Lanka': return 'Democratic Socialist Republic of Sri Lanka'; break;
+        case 'Congo (Kinshasa)': return 'Kinshasa'; break;
+        case 'Congo (Brazzaville)': return 'Brazzaville'; break;
+        case 'Cote d\'Ivoire': return 'Ivory Coast';
+        case 'Bahamas, The': return 'Bahamas'; break;
+        case 'Gambia, The': return 'Gambia'; break;
+        default: return countryName; break;
+    }
+}
+
+function getPopulation(country) {
+    const info = countriesInfo.find(countryInfo => countryInfo.name == normalizedName(country));
+    return info ? info.population : '';
+}
+
+function getCountryCode(country) {
+    const info = countriesInfo.find(countryInfo => countryInfo.name == normalizedName(country));
+    return info ? info.alpha2Code : '';
+}
+
+function formatData(data, country) {
+    if (Object.keys(data)[0] == 'All') {
+        let population = data.All.population;
+        if (! population) {
+            population = getPopulation(country);
+        }
+        return {
+            name: normalizedName(country),
+            originalName: country,
+            population: population,
+            confirmed: data.All.confirmed,
+            deaths: data.All.deaths,
+            recovered: data.All.recovered,
+            countryCode: getCountryCode(country)
+        };
+    } else {
+        let formattedData = [];
+        Object.keys(data).map(country => {
+            let population = data[country].All.population;
+            if (! population) {
+                population = getPopulation(country);
+            }
+            formattedData.push({
+                name: normalizedName(country),
+                originalName: country,
+                population: population,
+                confirmed: data[country].All.confirmed,
+                deaths: data[country].All.deaths,
+                recovered: data[country].All.recovered,
+                countryCode: getCountryCode(country)
+            });
+        });
+        return formattedData;
+    }
 }
 
 const cache = function (req, res, next) {
@@ -61,7 +124,7 @@ app.get('/history', async (req, res) => {
 
     const response = await axios.get(url);
 
-    res.send(response.data);
+    res.send(formatData(response.data, country));
 
     writeCache(getFilePath(req.originalUrl), response.data);
 });
@@ -75,7 +138,8 @@ app.get('/cases', async (req, res) => {
 
     const response = await axios.get(url);
 
-    res.send(response.data);
+
+    res.send(formatData(response.data, req.query.country));
 
     writeCache(getFilePath(req.originalUrl), response.data);
 });
